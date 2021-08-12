c3ef356cb5ecc17ed10e27d71f2a021972566c48
kicknback <joshua.borreli@gmail.com>
2021-08-12 14:38:45 -0500

    Completed basic CRUD operations for postscontroller

--- src/main/java/com/codeup/blogapp/web/PostsController.java
+++ src/main/java/com/codeup/blogapp/web/PostsController.java
@@ -1,10 +1,7 @@
 package com.codeup.blogapp.web;
 
 import com.codeup.blogapp.data.Post;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +19,32 @@ public class PostsController {
         }};
     }
 
-    @GetMapping("{id}")
-    private Post getPostById(@PathVariable Long id) {
-        // /api/posts/1
-        if (id == 1) {
-            return new Post(1L, "A new post", "this is a brilliant post. 10/10");
-        } else {
-            return null;
-        }
+//    @GetMapping("{id}")
+//    private Post getPostById(@PathVariable Long id) {
+//        // /api/posts/1
+//        if (id == 1) {
+//            return new Post(1L, "A new post", "this is a brilliant post. 10/10");
+//        } else {
+//            return null;
+//        }
+//    }
+
+    @PostMapping
+    private void createPost(@RequestBody Post newPost) {
+        System.out.println(newPost.getTitle());
+        System.out.println(newPost.getContent());
+    }
+
+    @PutMapping("{id}")
+    private void updatePost(@PathVariable Long id, @RequestBody Post post) {
+        System.out.println(post.getTitle());
+        System.out.println(post.getContent());
+        System.out.println(id);
+    }
+
+    @DeleteMapping("{id}")
+    private void deletePost(@PathVariable Long id) {
+        System.out.printf("Movie %s was deleted.", id);
     }
 
 }
