ce1217aeed5f758d24c17f18f362e955fa803edd
kicknback <joshua.borreli@gmail.com>
2021-08-12 19:38:49 -0500

    Added input form for posts

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -4,9 +4,25 @@ export default function PostIndex(props) {
             <h1>Posts Page</h1>
         </header>
         <main>
-            <div>
-                
-            </div>
+            
+            <form>
+
+                <div class="form-group">
+                    <label for="postid">Post ID</label>
+                    <input type="text" class="form-control" id="postid" placeholder="(Enter number)">
+                </div>
+                <div class="form-group">
+                    <label for="post-title">Title</label>
+                    <input type="text" class="form-control" id="post-title" placeholder="'Very cool blog'...">
+                </div>
+                <div class="form-group">
+                    <label for="post-content">Content</label>
+                    <textarea class="form-control" id="post-content" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+
+            </form>
+            
             <div>
                 ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
             </div>
