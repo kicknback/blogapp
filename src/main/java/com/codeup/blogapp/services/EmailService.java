74356e83dcf863555c5ef3ecec4e1a5dcf54e63d
kicknback <joshua.borreli@gmail.com>
2021-08-24 13:50:13 -0500

    Adding email service class and dependency

---
+++ src/main/java/com/codeup/blogapp/services/EmailService.java
@@ -0,0 +1,37 @@
+package com.codeup.blogapp.services;
+
+import com.codeup.blogapp.data.posts.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void prepareAndSend(Post post, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        // msg.setFrom(from);
+        msg.setFrom("joshua.borreli@gmail.com");
+        // msg.setTo(post.getUser().getEmail());
+        msg.setTo("joshua.borreli@yahoo.com");
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+}
